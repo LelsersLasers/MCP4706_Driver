bfbb21c84b95c632cf13dba6681f5c6dd9365197
Millan Kumar <millankumar@gmail.com>
2025-02-15 11:02:59 -0500

    Trying to pass Wire object so it is not forced to use wire1

--- Adafruit_MCP4706.cpp
+++ Adafruit_MCP4706.cpp
@@ -29,9 +29,12 @@ Adafruit_MCP4706::Adafruit_MCP4706() { }
 	@brief  Setups the HW
 */
 /**************************************************************************/
-void Adafruit_MCP4706::begin(uint8_t addr) {
+void Adafruit_MCP4706::begin(uint8_t addr, TwoWire &wire) {
 	_i2caddr = addr;
-	Wire.begin();
+  _wire = &wire;
+
+  // Also, user needs to setSCL and setSDA before calling begin
+	// Wire.begin(); 
 }
  
 /**************************************************************************/
@@ -44,13 +47,13 @@ void Adafruit_MCP4706::begin(uint8_t addr) {
 		the DAC's input voltage and its output voltage.
 */
 /**************************************************************************/
-void Adafruit_MCP4706::setVoltage( uint8_t output) {
+void Adafruit_MCP4706::setVoltage(uint8_t output) {
 	uint8_t twbrback = TWBR;
 	TWBR = 12; // 400 khz
 	// TWBR = 72; // 100 khz
-	Wire.beginTransmission(_i2caddr);
-	Wire.write(0);      // First Byte 0
-	Wire.write(output); // Second byte: Data bits          (D7.D6.D5.D4.D3.D2.D1.D0)
-	Wire.endTransmission();
+	_wire.beginTransmission(_i2caddr);
+	_wire.write(0);      // First Byte 0
+	_wire.write(output); // Second byte: Data bits          (D7.D6.D5.D4.D3.D2.D1.D0)
+	_wire.endTransmission();
 	TWBR = twbrback;
 }
