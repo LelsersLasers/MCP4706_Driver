b0b895f9aa0af7d114ce123d53883014a50e0ddb
Millan Kumar <millankumar@gmail.com>
2025-02-15 10:52:44 -0500

    Updating header

--- Adafruit_MCP4706.cpp
+++ Adafruit_MCP4706.cpp
@@ -1,24 +1,9 @@
 /**************************************************************************/
 /*! 
-    @file     Adafruit_MCP4706.cpp
-    @author   K.Townsend (Adafruit Industries)
-	@license  BSD (see license.txt)
-	
-        Modified for MCP4706 by Pio Baettig
-
-	I2C Driver for Microchip's MCP4706 I2C DAC
-
-	This is a library for the MCP4706 8-bit DAC modified from
-        Adafruit MCP4725 library
-	----> https://www.adafruit.com/products/???
-		
-	Adafruit invests time and resources providing this open source code, 
-	please support Adafruit and open-source hardware by purchasing 
-	products from Adafruit!
-
-	@section  HISTORY
-
-    v1.0 - First release
+  Original author:
+  - K.Townsend (Adafruit Industries)
+  - Modified for MCP4706 by Pio Baettig
+  - Now modifed by Millan kumar for PER HIL 2.0 2025 usage
 */
 /**************************************************************************/
 
