d52424c286c524ad188598d5c2e6388bc658be85
Millan Kumar <millankumar@gmail.com>
2025-02-15 10:52:38 -0500

    Formatting

--- Adafruit_MCP4706.cpp
+++ Adafruit_MCP4706.cpp
@@ -21,6 +21,7 @@
     v1.0 - First release
 */
 /**************************************************************************/
+
 #if ARDUINO >= 100
  #include "Arduino.h"
 #else
@@ -33,41 +34,38 @@
 
 /**************************************************************************/
 /*! 
-    @brief  Instantiates a new MCP4706 class
+	@brief  Instantiates a new MCP4706 class
 */
 /**************************************************************************/
-Adafruit_MCP4706::Adafruit_MCP4706() {
-}
+Adafruit_MCP4706::Adafruit_MCP4706() { }
 
 /**************************************************************************/
 /*! 
-    @brief  Setups the HW
+	@brief  Setups the HW
 */
 /**************************************************************************/
 void Adafruit_MCP4706::begin(uint8_t addr) {
-  _i2caddr = addr;
-  Wire.begin();
-
+	_i2caddr = addr;
+	Wire.begin();
 }
  
 /**************************************************************************/
 /*! 
-    @brief  Sets the output voltage to a fraction of source vref.  (Value
-            can be 0..255)
+	@brief  Sets the output voltage to a fraction of source vref. (Value
+		can be 0..255)
 
-    @param[in]  output
-                The 8-bit value representing the relationship between
-                the DAC's input voltage and its output voltage.
+	@param[in]  output
+		The 8-bit value representing the relationship between
+		the DAC's input voltage and its output voltage.
 */
 /**************************************************************************/
-void Adafruit_MCP4706::setVoltage( uint8_t output)
-{
-  uint8_t twbrback = TWBR;
-    TWBR = 12; // 400 khz
-  //  TWBR = 72; // 100 khz
-  Wire.beginTransmission(_i2caddr);
-  Wire.write(0);            // First Byte 0
-  Wire.write(output);       // Second byte: Data bits          (D7.D6.D5.D4.D3.D2.D1.D0)
-  Wire.endTransmission();
-  TWBR = twbrback;
+void Adafruit_MCP4706::setVoltage( uint8_t output) {
+	uint8_t twbrback = TWBR;
+	TWBR = 12; // 400 khz
+	// TWBR = 72; // 100 khz
+	Wire.beginTransmission(_i2caddr);
+	Wire.write(0);      // First Byte 0
+	Wire.write(output); // Second byte: Data bits          (D7.D6.D5.D4.D3.D2.D1.D0)
+	Wire.endTransmission();
+	TWBR = twbrback;
 }
