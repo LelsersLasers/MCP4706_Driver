a285ce698e47d2262de5c60187d295ca9bd3dc77
Millan Kumar <millankumar@gmail.com>
2025-02-15 11:05:50 -0500

    Tabs/spaces fix

--- Adafruit_MCP4706.cpp
+++ Adafruit_MCP4706.cpp
@@ -1,9 +1,9 @@
 /**************************************************************************/
 /*! 
-  Authors:
-  - Original: K.Townsend (Adafruit Industries)
-  - Modified for MCP4706 by Pio Baettig
-  - Now modifed by Millan kumar for PER HIL 2.0 2025 usage
+    Authors:
+    - Original: K.Townsend (Adafruit Industries)
+    - Modified for MCP4706 by Pio Baettig
+    - Now modifed by Millan kumar for PER HIL 2.0 2025 usage
 */
 /**************************************************************************/
 
@@ -19,41 +19,41 @@
 
 /**************************************************************************/
 /*! 
-	@brief  Instantiates a new MCP4706 class
+    @brief  Instantiates a new MCP4706 class
 */
 /**************************************************************************/
 Adafruit_MCP4706::Adafruit_MCP4706() { }
 
 /**************************************************************************/
 /*! 
-	@brief  Setups the HW
+    @brief  Setups the HW
 */
 /**************************************************************************/
 void Adafruit_MCP4706::begin(uint8_t addr, TwoWire &wire) {
-	_i2caddr = addr;
-  _wire = &wire;
+    _i2caddr = addr;
+    _wire = &wire;
 
-  // Also, user needs to setSCL and setSDA before calling begin
-	// Wire.begin(); 
+    // Also, user needs to setSCL and setSDA before calling begin
+    // Wire.begin(); 
 }
  
 /**************************************************************************/
 /*! 
-	@brief  Sets the output voltage to a fraction of source vref. (Value
-		can be 0..255)
+    @brief  Sets the output voltage to a fraction of source vref. (Value
+        can be 0..255)
 
-	@param[in]  output
-		The 8-bit value representing the relationship between
-		the DAC's input voltage and its output voltage.
+    @param[in]  output
+        The 8-bit value representing the relationship between
+        the DAC's input voltage and its output voltage.
 */
 /**************************************************************************/
 void Adafruit_MCP4706::setVoltage(uint8_t output) {
-	uint8_t twbrback = TWBR;
-	TWBR = 12; // 400 khz
-	// TWBR = 72; // 100 khz
-	_wire->beginTransmission(_i2caddr);
-	_wire->write(0);      // First Byte 0
-	_wire->write(output); // Second byte: Data bits          (D7.D6.D5.D4.D3.D2.D1.D0)
-	_wire->endTransmission();
-	TWBR = twbrback;
+    uint8_t twbrback = TWBR;
+    TWBR = 12; // 400 khz
+    // TWBR = 72; // 100 khz
+    _wire->beginTransmission(_i2caddr);
+    _wire->write(0);      // First Byte 0
+    _wire->write(output); // Second byte: Data bits (D7.D6.D5.D4.D3.D2.D1.D0)
+    _wire->endTransmission();
+    TWBR = twbrback;
 }
